b2cb8cba6a8bac10c459e128a5583b1a323dc131
Jose-pablo-faundez <josfaundez@alumnos.uai.cl>
2025-04-23 22:35:08 -0400

    P00-35 Se agregó en app.tsx la extensión para utilizar API con OpenFoodFacts

--- App.tsx
+++ App.tsx
@@ -55,13 +55,48 @@ const AppNavigator = () => {
 };
 
 export default function App() {
+  const [state, setState] = React.useState({
+    loading: false,
+    foods: [],
+    url: 'https://world.openfoodfacts.org/api/v3/product/50184453.json',
+  });
+
+React.useEffect(() => {
+  let isMounted = true; // Indicador para evitar fugas de memoria
+  const url = state.url; // Guardar el valor actual de state.url
+
+  const getFoods = async () => {
+  try {
+      setState((prevState) => ({ ...prevState, loading: true }));
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log('Datos recibidos de la API:', data); // Verificar los datos en la consola
+      if (isMounted) {
+          setState((prevState) => ({ ...prevState, loading: false, foods: data }));
+      }
+  } catch (error) {
+      console.error('Error fetching data:', error);
+      if (isMounted) {
+          setState((prevState) => ({ ...prevState, loading: false }));
+      }
+  }
+};
+
+  getFoods();
+
+  return () => {
+    isMounted = false; // Limpiar al desmontar
+  };
+}, [state.url]);
+
+
   return (
     <SafeAreaProvider>
       <ThemeProvider>
         <AuthProvider>
-          <NavigationContainer>
-            <AppNavigator />
-          </NavigationContainer>
+              <NavigationContainer>
+                <AppNavigator />
+              </NavigationContainer>
         </AuthProvider>
       </ThemeProvider>
     </SafeAreaProvider>
